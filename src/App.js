d07f702715d5fd3e11943015a621808d111bf718
Mark Cruz <markaycruz@gmail.com>
2022-02-05 00:31:28 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect ,BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Redirect , HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Profile from './components/Profile';
